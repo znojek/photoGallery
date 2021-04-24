77824f1ad3d220a328c36a4ba9d90dd70cea4996
Maciej Znojkiewicz <37136052+znojek@users.noreply.github.com>
2021-04-24 23:23:14 +0200

    new update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 
 import Title from './Components/Title';
 import SearchBars from './Components/SearchBars'
+import "./Styles/Title.css";
+import "./Styles/Inputs.css";
+import "./Styles/Frame.css";
+import "./Styles/DisplayPhoto.css";
+import MainFrame from './Components/MainFrame';
+
 
 
 function App() {
   return (
     <div >
       <Title />
+      <MainFrame/>
       <SearchBars />
     
     </div>
